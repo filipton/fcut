43abf31b072748aa488d97632382f9834c51c890
filipton <filipton12@gmail.com>
2022-12-01 23:04:25 +0100

    Added dockerfiles for go and node

--- build.sh
+++ build.sh
@@ -15,6 +15,22 @@ cd "$currentDir"
 echo "DOTNET build done! (Docker image: filipton/fcut-dotnet:latest)"
 
 
+echo "Starting NODE build..."
+cd ./Node
+bash ./build.sh
+cd "$currentDir"
+echo "NODE build done! (Docker image: filipton/fcut-node:latest)"
+
+
+echo "Starting GO build..."
+cd ./Golang
+bash ./build.sh
+cd "$currentDir"
+echo "GO build done! (Docker image: filipton/fcut-go:latest)"
+
+
 echo "Deploying images to docker registry..."
 docker push filipton/fcut-rust:latest
 docker push filipton/fcut-dotnet:latest
+docker push filipton/fcut-node:latest
+docker push filipton/fcut-go:latest
